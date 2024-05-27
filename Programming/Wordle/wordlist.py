dedd320641d7474294982b3378479d603fd255f1
jefjacobs00 <jacobsjef16@gmail.com>
2024-05-27 20:42:03 +0200

    cleaning up

--- Programming/Wordle/wordlist.py
+++
@@ -1,8 +0,0 @@
-file = open("wordlist.10000")
-words = file.read().split('\n')
-
-file2 = open("wordlist.filtered", "w")
-for word in words:
-    if 5 == len(word):
-        file2.write(f"{word}\n")
-
